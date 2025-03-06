634d6e4a814ff8b31914895830ac94a862b7b0ad
PantelisGavalas <pantelis.g7@gmail.com>
2025-03-06 10:07:10 +0100

    Initial commit message for creating n^2 solution of first problem '1_two_sum' along with the way to have test cases to test it in an IDE. For next problems, only the Solution class without main method for testing will be presented. Whole code can be put to LeetCode problem for testing and submission.

---
+++ Java/1_two_sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int[] result = new int[2];
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return new int[0];  // never used
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();     // create instance of Solution
+
+        // Test case 1
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Test case 1: [" + result1[0] + " " + result1[1] + "]");
+
+        // Test case 2
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Test Case 2: [" + result2[0] + ", " + result2[1] + "]");
+
+        // Test case 3
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Test Case 3: [" + result3[0] + ", " + result3[1] + "]");
+    }
+}
