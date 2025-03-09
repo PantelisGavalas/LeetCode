29738d75c61b8995178521b3723546ace4df04d2
PantelisGavalas <pantelis.g7@gmail.com>
2025-03-10 01:35:57 +0200

    Solution for problem 11 'Max Water Container' using techinque 'Two pointers'.

---
+++ Java/11_container_with_most_water/11_container_with_most_water.java
@@ -0,0 +1,18 @@
+import java.util.Math;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+            int currWater = (right - left) * Math.min(height[right], height[left]);
+            maxWater = Math.max(maxWater, currWater);
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+
+        return maxWater;
+    }
+}
