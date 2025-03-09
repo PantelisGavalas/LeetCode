2143cc3b11418e45bc9837a0974630a5640afb22
PantelisGavalas <pantelis.g7@gmail.com>
2025-03-09 23:18:30 +0200

    Added explanation on problem 27 solution using the 'Two Pointers' technique.

--- Java/27_remove_element/27_remove_element.java
+++ Java/27_remove_element/27_remove_element.java
@@ -1,6 +1,9 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        // Used the "Two Pointers - Fast & Slow pointers" method
+        // slow pointer moving under satisfied condition
         int i = 0;
+        // fast pointer iterating our int[] array
         for (int number : nums) {
             if (number != val) {
                 nums[i++] = number;
